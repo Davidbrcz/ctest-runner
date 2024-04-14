2bd05e872d6a5252b0f931d9a89bf50a2e3fa7e2
Davidbrcz <davidbrcz@gmail.com>
2024-04-14 21:53:55 +0200

    WUP

--- ctest-runner.el
+++ ctest-runner.el
@@ -5,11 +5,11 @@
 (require 'treemacs-treelib)
 
 
-(defun fresh-buffer (bufname)
+(defun ctest-runner--fresh-buffer (bufname)
   (--when-let (get-buffer bufname) (kill-buffer it))
   (get-buffer-create bufname))
 
-(defun get-ctests-json-output(dir)
+(defun ctest-runner--get-ctests-json-output(dir)
   (let* ((ctest-json-output-buffer-name (format "*ctest-list-tests: %s*" dir))
          (default-directory dir))
     (--when-let (get-buffer ctest-json-output-buffer-name) (kill-buffer it))
@@ -20,32 +20,32 @@
         (json-parse-buffer :object-type 'plist :array-type 'list)))))
 
 
-(defun get-test-filepath(atest)
+(defun ctest-runner--get-test-filepath(atest)
   (car (plist-get atest :command)))
 
-(defun get-test-filename(atest)
+(defun ctest-runner--get-test-filename(atest)
   (file-name-base (car (plist-get atest :command))))
 
-(defun get-ctests-tests-files()
-  (let* ((json-structure (get-ctests-json-output (projectile-compilation-dir)))
+(defun ctest-runner--get-ctests-tests-files()
+  (let* ((json-structure (ctest-runner--get-ctests-json-output (projectile-compilation-dir)))
          (tests (plist-get json-structure ':tests)))
     (->> tests
-         (-map 'get-test-filepath)
+         (-map 'ctest-runner--get-test-filepath)
          (-distinct)
          (-map (lambda (filepath) (file-name-base filepath)))
          )))
 
-(defun treemacs-showcase--tests-by-file (filename)
-  (let* ((json-structure (get-ctests-json-output (projectile-compilation-dir)))
+(defun ctest-runner--tests-by-file (filename)
+  (let* ((json-structure (ctest-runner--get-ctests-json-output (projectile-compilation-dir)))
          (tests (plist-get json-structure ':tests)))
     (->> tests
          (-filter
           (lambda (atest)
-            (let* ((afilename (get-test-filename atest)))
+            (let* ((afilename (ctest-runner--get-test-filename atest)))
               (string-equal filename  afilename))))
          )))
 
-(defun treemacs-showcase-RET-buffer-action (&optional _)
+(defun ctest-runner--RET-single-test-action (&optional _)
   (let* ((test-command (-some-> (treemacs-current-button)
                          (treemacs-button-get :testcmd)))
          (test-name (-some-> (treemacs-current-button)
@@ -53,7 +53,7 @@
          (binary (car test-command))
          (arguments (cdr test-command))
          (out-buffer-name (format "*%s:%s*" binary test-name))
-         (out-buffer (fresh-buffer out-buffer-name)))
+         (out-buffer (ctest-runner--fresh-buffer out-buffer-name)))
     ;; use apply to be able to give arguments (a list) where
     ;; a direct call to call-process expects individual elements
     (apply #'call-process binary nil out-buffer nil arguments)
@@ -63,19 +63,25 @@
       (pop-to-buffer out-buffer))
     ))
 
+(defun treemacs-showcase-RET-buffer-action (&optional _)
+  (let ((buffer (-some-> (treemacs-current-button)
+                  (treemacs-button-get :buffer))))
+    (message "ret" )))
 
-(defun treemacs-showcase-visit-buffer-action (btn)
+
+(defun ctest-runner-visit-buffer-action (btn)
   (let ((buffer (treemacs-safe-button-get btn :buffer)))
-    (message "visit on %s" buffer)))
+    (message "visit on")))
 
 (treemacs-define-expandable-node-type showcase-test-file
   :closed-icon "+ "
   :open-icon "- "
   :label (propertize item 'face 'font-lock-variable-name-face)
   :key item
-  :children (treemacs-showcase--tests-by-file item)
+  :children (ctest-runner--tests-by-file item)
   :child-type 'showcase-single-test
   :more-properties `(:major-mode ,item)
+  :ret-action #'treemacs-showcase-RET-buffer-action
   :on-expand (message "Expanding node with key %s" (treemacs-button-get btn :key))
   :on-collapse (message "Collapsing node with key %s" (treemacs-button-get btn :key)))
 
@@ -86,17 +92,17 @@
   :label (propertize (plist-get item ':name) 'face 'font-lock-string-face)
   :key item
   :more-properties `(:testcmd ,(plist-get item ':command) :testname ,(plist-get item ':name))
-  :visit-action #'treemacs-showcase-visit-buffer-action
-  :ret-action #'treemacs-showcase-RET-buffer-action)
+  :visit-action #'ctest-runner-visit-buffer-action
+  :ret-action #'ctest-runner--RET-single-test-action)
 
 (treemacs-define-variadic-entry-node-type showcase-tests-files-variadic
   :key 'showcase-tests-files-variadic
-  :children  (get-ctests-tests-files)
+  :children  (ctest-runner--get-ctests-tests-files)
   :child-type 'showcase-test-file)
 
 (defun showcase-tests ()
   (interactive)
   (let ((bufname (format "*Showcase tests %s*" (projectile-project-name))))
-    (let ((buf (fresh-buffer bufname)))
+    (let ((buf (ctest-runner--fresh-buffer bufname)))
       (pop-to-buffer buf)
       (treemacs-initialize showcase-tests-files-variadic))))
